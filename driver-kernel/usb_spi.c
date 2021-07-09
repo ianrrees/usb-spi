9730436dc02768a7e7c7bc1b90a0daa076abfac6
Ian Rees <code@ianrees.nz>
2021-07-10 11:04:06 +1200

    Register a (dummy) SPI master
    Need to use spi_new_device() to register connected devices

--- driver-kernel/usb_spi.c
+++ driver-kernel/usb_spi.c
@@ -6,24 +6,45 @@
 #include <linux/slab.h>
 #include <linux/module.h>
 #include <linux/usb.h>
+#include <linux/spi/spi.h>
 
-struct usb_spi {
+struct usb_spi_device {
 	struct usb_device *usb_dev;
+	struct spi_master *spi_master;
 };
+
+static int usb_spi_setup(struct spi_device *spi)
+{
+	printk(KERN_INFO "usb_spi_setup()"); // TODO
+	return 0;
+}
+
+static void usb_spi_cleanup(struct spi_device *spi)
+{
+	printk(KERN_INFO "usb_spi_cleanup()"); // TODO
+}
+
+static int usb_spi_transfer_one_message(struct spi_master *master, struct spi_message *mesg)
+{
+	struct usb_spi_device *usb_spi = spi_master_get_devdata(master);
+
+	dev_info(&usb_spi->usb_dev->dev, "usb_spi_transfer_one_message()"); // TODO
+	return 0;
+}
 
 static int usb_spi_probe(struct usb_interface *usb_if, const struct usb_device_id *id)
 {
 	struct usb_device *usb_dev = NULL;
-	struct usb_spi *usb_spi = NULL;
+	struct usb_spi_device *usb_spi = NULL;
+	struct spi_master *spi_master = NULL;
+	int ret = 0;
 
 	usb_dev = usb_get_dev(interface_to_usbdev(usb_if));
 	if (!usb_dev) {
 		return -ENODEV;
 	}
 
-	// For now, just assume the table only gives us valid interfaces
-
-	dev_info(&usb_dev->dev, "New USP-SPI device %04X:%04X", id->idVendor, id->idProduct);
+	// For now, just assume the table only gives us valid USB interfaces
 
 	usb_spi = kzalloc(sizeof(*usb_spi), GFP_KERNEL);
 	if (!usb_spi) {
@@ -32,15 +53,60 @@ static int usb_spi_probe(struct usb_interface *usb_if, const struct usb_device_i
 
 	usb_spi->usb_dev = usb_dev;
 
+	spi_master = spi_alloc_master(&usb_spi->usb_dev->dev, sizeof(usb_spi));
+	if (!spi_master) {
+		ret = -ENOMEM;
+		goto err;
+	}
+
+	spi_master_set_devdata(spi_master, usb_spi);
+
+	// TODO get parameters from the device
+	spi_master->min_speed_hz = 1000;
+	spi_master->max_speed_hz = 1000;
+
+	spi_master->bus_num = -1; // dynamic
+	spi_master->num_chipselect = 1; // TODO hook up GPIO
+	spi_master->mode_bits = SPI_MODE_0 | SPI_MODE_1 | SPI_MODE_2 | SPI_MODE_3;
+
+	spi_master->flags = 0;
+	spi_master->setup = usb_spi_setup;
+	spi_master->cleanup = usb_spi_cleanup;
+	spi_master->transfer_one_message = usb_spi_transfer_one_message;
+
+	ret = spi_register_master(spi_master);
+	if (ret) {
+		spi_master_put(spi_master); // Typically handled by spi_unregister_master
+		spi_master = NULL;
+		goto err;
+	}
+	usb_spi->spi_master = spi_master;
+
+	dev_info(&usb_spi->usb_dev->dev, "New USP-SPI device %04X:%04X SPI %d",
+	         id->idVendor, id->idProduct, spi_master->bus_num);
+
 	usb_set_intfdata(usb_if, usb_spi);
-	return 0;
+	return ret;
+
+err:
+	if (spi_master) {
+		spi_unregister_master(spi_master);
+	}
+	if (usb_spi) {
+		usb_put_dev(usb_spi->usb_dev);
+		kfree(usb_spi);
+	}
+
+	return ret;
 }
 
 static void usb_spi_disconnect(struct usb_interface *interface)
 {
-	struct usb_spi *usb_spi = usb_get_intfdata(interface);
+	struct usb_spi_device *usb_spi = usb_get_intfdata(interface);
 
 	dev_info(&usb_spi->usb_dev->dev, "USB-SPI disconnected");
+
+	spi_unregister_master(usb_spi->spi_master);
 
 	usb_put_dev(usb_spi->usb_dev);
 	kfree(usb_spi);
