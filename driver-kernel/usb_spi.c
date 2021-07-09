de0936984c3fa8039a762f4dc34b79ef447effe3
Ian Rees <code@ianrees.nz>
2021-07-10 09:05:09 +1200

    Apply some of Kernel style guide
    https://www.kernel.org/doc/html/latest/process/coding-style.html
    
    Except the bit that recommends no braces on single-line conditionals.

--- driver-kernel/usb_spi.c
+++ driver-kernel/usb_spi.c
@@ -7,29 +7,38 @@
 #include <linux/module.h>
 #include <linux/usb.h>
 
-typedef struct {
+struct usb_spi {
 
-} usb_spi_t;
+};
 
-static int usb_spi_probe(struct usb_interface *probing_interface, const struct usb_device_id *id)
+static int usb_spi_probe(struct usb_interface *usb_if, const struct usb_device_id *id)
 {
-    printk(KERN_INFO "USB-SPI candidate device (%04X:%04X) found\n", id->idVendor, id->idProduct);
+	struct usb_device *usb_dev = NULL;
+	struct usb_spi *usb_spi = NULL;
 
-    // For now, just assume the table only gives us valid interfaces
-    usb_spi_t *usb_spi = usb_spi = kzalloc(sizeof(usb_spi_t), GFP_KERNEL);
-    if (!usb_spi) {
-        return -ENOMEM;
-    }
+	usb_dev = usb_get_dev(interface_to_usbdev(usb_if));
+	if (!usb_dev) {
+		return -ENODEV;
+	}
 
-    // TODO	- do we need to call usb_get_intf() to increase refcount on the interface?
+	// For now, just assume the table only gives us valid interfaces
+
+	dev_info(&usb_dev->dev, "New USP-SPI device %04X:%04X", id->idVendor, id->idProduct);
+
+	usb_spi = kzalloc(sizeof(*usb_spi), GFP_KERNEL);
+	if (!usb_spi) {
+		return -ENOMEM;
+	}
 
     usb_set_intfdata(probing_interface, usb_spi);
-    return 0;
+
+	usb_set_intfdata(usb_if, usb_spi);
+	return 0;
 }
 
 static void usb_spi_disconnect(struct usb_interface *interface)
 {
-    usb_spi_t *usb_spi = usb_get_intfdata(interface);
+	struct usb_spi *usb_spi = usb_get_intfdata(interface);
 
     printk(KERN_INFO "USB-SPI removed\n");
     
@@ -38,28 +47,28 @@ static void usb_spi_disconnect(struct usb_interface *interface)
 
 static struct usb_device_id usb_spi_id[] =
 {
-    // TODO This and/or poll() could check the class/subclass/protocol
-    {USB_DEVICE_INTERFACE_NUMBER((DEV_VID), (DEV_PID), (DEV_INTERFACE))},
-    {}
+	// TODO This and/or poll() could check the class/subclass/protocol
+	{USB_DEVICE_INTERFACE_NUMBER((DEV_VID), (DEV_PID), (DEV_INTERFACE))},
+	{}
 };
 MODULE_DEVICE_TABLE (usb, usb_spi_id);
 
 static struct usb_driver usb_spi_driver =
 {
-    .name = "usb_spi",
-    .id_table = usb_spi_id,
-    .probe = usb_spi_probe,
-    .disconnect = usb_spi_disconnect,
+	.name = "usb_spi",
+	.id_table = usb_spi_id,
+	.probe = usb_spi_probe,
+	.disconnect = usb_spi_disconnect,
 };
 
 static int __init usb_spi_init(void)
 {
-    return usb_register(&usb_spi_driver);
+	return usb_register(&usb_spi_driver);
 }
 
 static void __exit usb_spi_exit(void)
 {
-    usb_deregister(&usb_spi_driver);
+	usb_deregister(&usb_spi_driver);
 }
 
 module_init(usb_spi_init);
