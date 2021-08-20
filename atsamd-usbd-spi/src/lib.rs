99a205a10f0a223636dec2310efed8c540171a65
Ian Rees <code@ianrees.nz>
2021-08-20 14:00:18 +1200

    Move firmware SpiDevice stuff to a module

--- atsamd-usbd-spi/src/lib.rs
+++ atsamd-usbd-spi/src/lib.rs
@@ -4,12 +4,7 @@ extern crate atsamd_hal;
 extern crate defmt_rtt;
 
 use atsamd_hal::{
-    gpio::v2::*,
-    hal::{
-        // embedded_hal stuff
-        digital::v2::OutputPin,
-        spi::{self, Mode},
-    },
+    hal::spi::{self, Mode}, // embedded_hal stuff
     prelude::*,
     sercom::v2::spi::{
         Config, EightBit, Error as SpiError, Errors as SpiErrors, Flags, Master, Rx, Spi, Tx,
@@ -17,6 +12,9 @@ use atsamd_hal::{
     },
     typelevel::NoneT,
 };
+
+mod spi_device;
+pub use spi_device::*;
 
 // TODO use const generics, so our customers don't need to see this
 pub use bbqueue::consts::*;
@@ -41,107 +39,6 @@ const USB_SPI_PROTOCOL_0: u8 = 0x00; // TODO push up to a protocol crate
 
 /// SPI reads require writes, use this when there isn't other data to write.
 const USB_SPI_TX_FILL: u8 = 0x00;
-
-/// Describes an SPI-connected peripheral device
-///
-/// This is a trait because the chip select handling might be more complicated
-/// than toggling a GPIO pin, and because we might support passing a device-
-/// specific struct up to the device's driver on the USB host.
-pub trait SpiDevice {
-    /// Assert the device's chip select
-    fn select(&mut self);
-
-    /// Deassert the device's chip select
-    fn deselect(&mut self);
-
-    /// NOP for chips that don't have a reset
-    fn assert_reset(&mut self);
-    
-    /// NOP for chips that don't have a reset
-    fn deassert_reset(&mut self);
-
-    /// modalias is how Linux SPI drivers associate devices with controllers
-    fn modalias(&self) -> &'static str;
-
-    /// Returns the maximum clock speed supported by the device, in Hz
-    fn max_clock_speed_hz(&self) -> u32;
-
-    /// Returns the capabilities bitmask associated with the device
-    fn capabilities(&self) -> SpiDeviceCapabilities;
-}
-
-// TODO add interrupt
-// TODO change this to accept embedded_hal's output pin
-pub struct BasicSpiDevice<P, R>
-where
-    P: AnyPin<Mode = PushPullOutput>,
-    R: AnyPin<Mode = PushPullOutput>, // TODO change this to an OptionalPin
-{
-    cs_pin: P,
-    reset_pin: R,
-    modalias: Option<&'static str>,
-    max_clock_speed_hz: u32,
-    capabilities: SpiDeviceCapabilities,
-}
-
-impl<P, R> BasicSpiDevice<P, R>
-where
-    P: AnyPin<Mode = PushPullOutput> + OutputPin,
-    R: AnyPin<Mode = PushPullOutput> + OutputPin,
-{
-    /// Construct an SPI device that has both a chip select and reset pin
-    ///
-    /// reset_pin is assumed to be active low
-    pub fn new(
-        cs_pin: impl Into<P>,
-        reset_pin: impl Into<R>,
-        modalias: Option<&'static str>,
-        max_clock_speed_hz: u32,
-        capabilities: SpiDeviceCapabilities,
-    ) -> Self {
-        Self {
-            cs_pin: cs_pin.into(),
-            reset_pin: reset_pin.into(),
-            modalias,
-            max_clock_speed_hz,
-            capabilities,
-        }
-    }
-}
-
-impl<P, R> SpiDevice for BasicSpiDevice<P, R>
-where
-    P: AnyPin<Mode = PushPullOutput> + OutputPin,
-    R: AnyPin<Mode = PushPullOutput> + OutputPin,
-{
-    fn select(&mut self) {
-        self.cs_pin.set_low().ok().unwrap();
-    }
-
-    fn deselect(&mut self) {
-        self.cs_pin.set_high().ok().unwrap();
-    }
-
-    fn assert_reset(&mut self) {
-        self.reset_pin.set_low().ok().unwrap();
-    }
-
-    fn deassert_reset(&mut self) {
-        self.reset_pin.set_high().ok().unwrap();
-    }
-
-    fn modalias(&self) -> &'static str {
-        self.modalias.unwrap_or("")
-    }
-
-    fn max_clock_speed_hz(&self) -> u32 {
-        self.max_clock_speed_hz
-    }
-
-    fn capabilities(&self) -> SpiDeviceCapabilities {
-        self.capabilities
-    }
-}
 
 pub type SpiDeviceList<'a> = [&'a mut dyn SpiDevice];
 
